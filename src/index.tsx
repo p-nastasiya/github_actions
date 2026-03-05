47dd1a10ff63b89654fb6706e7ec3b5b3f5c1fed
Alexandr <lvar-8@ya.ru>
2026-03-05 15:44:50 +0300

    Новый путь для деплоя

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app';
-import { Analytic } from './context/analityc';
+//import { Analytic } from './context/analityc';
 import './styles.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
 	<StrictMode>
-		<Analytic.Provider value={{ yandexId: 96728508, googleId: 'G-ZXWBHB1NR0' }}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Analytic.Provider>
+		<BrowserRouter
+			basename={process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/'}>
+			<App />
+		</BrowserRouter>
 	</StrictMode>
 );
